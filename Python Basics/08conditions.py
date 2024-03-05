19462978bb775adea50f46bdc2685ac909488df9
Jasmine Awatramani <42552965+jazzy28@users.noreply.github.com>
2024-03-05 20:24:40 +0530

    Add files via upload

---
+++ Python Basics/08conditions.py
@@ -0,0 +1,30 @@
+age = 24
+
+if age<=24:
+	print("young")
+elif age==25:
+	print("quarter life crisis")
+else: 
+	print("old")
+
+print("be kind")
+
+#Calculator
+
+a = int(input("enter the first number:"))
+b = int(input("enter the first number:"))
+x = ("enter the operation number:")
+
+if x == "+":
+	print(a + b)
+elif x == "-":
+	print(a - b)
+elif x == "*":
+	print(a * b)
+elif x == "/":
+	print(a / b)
+elif x == "%":
+	print(a % b)
+else:
+	print("Invalid operation")
+	
