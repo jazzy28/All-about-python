753225608edee8329bc5dce5c3fd4614833f8b22
Jasmine Awatramani <42552965+jazzy28@users.noreply.github.com>
2024-03-05 19:50:05 +0530

    Add files via upload

---
+++ Python Basics/07comparisonops.py
@@ -0,0 +1,13 @@
+# >, <, >=, <=, != (not)
+
+print(3 != 3) #as 3 is equal to 3, it will return False
+
+#it will return boolean
+
+print (3 != 2)
+
+#and, or, not
+print(2 > 3 or 2 > 1) #anyone of this if true, it will return true
+print(3 > 2  and 2 > 6) #will only be true if both conditions are true
+print(not 3 > 2) #it will be false as 3>2
+
